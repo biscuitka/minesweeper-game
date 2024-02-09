97cac9e1348825c9f401ff6d2f02398dd431ec2f
biscuitka <biscuitka@ya.ru>
2024-02-09 13:00:50 +0300

    init commit

---
+++ src/main/java/com/biscuitka/minesweepergame/MinesweeperGameApplication.java
@@ -0,0 +1,13 @@
+package com.biscuitka.minesweepergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperGameApplication.class, args);
+	}
+
+}
